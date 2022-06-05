336fed8a504797aa49dc24153f07952a038555fe
PeterLe0349 <103457196+PeterLe0349@users.noreply.github.com>
2022-06-05 17:44:15 -0500

    java scanner

---
+++ src/TestConsole.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class TestConsole {
+    public static void main(String[] args){
+//        String name = "codeup";
+//        System.out.printf("Hello there, %s. Nice to see you.%n", name);
+        String greeting = "Salutations";
+        String name = "codeup";
+        System.out.printf("%s,%n %s!", greeting, name);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter something: ");
+        String userInput = scanner.nextLine();
+        System.out.println("You entered --> \"" + userInput + "\" <--");
+
+
+    }
+}
