c795f001c0215eeee761fa2fef585c3a0955c9fd
PeterLe0349 <103457196+PeterLe0349@users.noreply.github.com>
2022-06-09 17:46:09 -0500

    up

--- src/TestTestTest.java
+++ src/TestTestTest.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class TestTestTest {
     public static void main(String[] args){
-        System.out.println("TEST");
+//        System.out.println(24%2 == 0);
+        Scanner sc=new Scanner(System.in);
+        System.out.println("================================");
+        for(int i=0;i<3;i++)
+        {
+            String s1=sc.next();
+            int x=sc.nextInt();
+            //Complete this line
+            System.out.printf("%15s %3d", s1, x);
+        }
+        System.out.println("================================");
+
     }
 }
